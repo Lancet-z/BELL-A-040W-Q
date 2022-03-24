459d9745d1722d1bb1e993ae85367d1aedc06aec
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-03-24 16:03:16 +0800

    Update and rename diy-part2.sh to part2.sh

--- part2.sh
+++ part2.sh
@@ -10,7 +10,6 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
@@ -25,3 +24,15 @@ sed -i "s/OpenWrt /迷宫旅人.ver $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" p
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
+# 修改默认wifi名称ssid为迷宫旅人
+sed -i 's/ssid=OpenWrt/ssid=迷宫旅人/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+#开启MU-MIMO
+sed -i 's/mu_beamformer=0/mu_beamformer=1/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+#wifi加密方式，没有是none，改为psk2
+sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+#wifi密码
+sed -i 's/key=1234567890/key=gds.2021/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
